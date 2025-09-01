f0b67a33780f6bb7f88fe197d0f73989071a0f50
CursedGreanbeen <m.adelina_2003@mail.com>
2025-09-02 03:56:38 +0500

    Function add_quote() updated

--- quotes/views.py
+++ quotes/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.http import JsonResponse
 from django.core.exceptions import ValidationError, PermissionDenied
-from django.views.decorators.http import require_POST
+from django.contrib import messages
+from django.shortcuts import render, redirect
 from .models import Quote, Source
 from .forms import AddQuoteForm
 import random
@@ -14,14 +15,38 @@ def index(request):
                   'quotes/index.html', context={'random': random_quote})
 
 
-def add_quote(request, q_text, q_source, q_weight):
+def add_quote(request):
     add_quote_form = AddQuoteForm()
-    source = q_source.strip().capitalize()
-    if source.quote_set.count() >= 3:
-        raise ValidationError('Из одного источника не более 3 цитат!')
-    else:
-        new_quote = Quote.objects.create(text=q_text, source=source, weight=q_weight)
-    return render(request, 'add_quote.html', {"form": add_quote_form})
+
+    if request.method == 'POST':
+        add_quote_form = AddQuoteForm(request.POST)
+        if add_quote_form.is_valid():
+            normalized_source = add_quote_form.quote_source.strip().capitalize()
+            quote_source = Source.objects.filter(name=normalized_source).first()
+
+            if not quote_source:
+                quote_source = Source.objects.create(name=normalized_source)
+                new_quote = Quote.objects.create(
+                    text=add_quote_form.quote_text,
+                    source=quote_source,
+                    weight=add_quote_form.quote_weight
+                )
+                messages.success(request, 'Quote added successfully!')
+                return redirect('index')
+
+            elif quote_source.quote_set.count() >= 3:
+                add_quote_form.add_error('source', '3 quotes max per each source!')
+
+            else:
+                new_quote = Quote.objects.create(
+                    text=add_quote_form.quote_text,
+                    source=quote_source,
+                    weight=add_quote_form.quote_weight
+                )
+                messages.success(request, 'Quote added successfully!')
+                return redirect('index')
+
+    return render(request, 'quotes/add_quote.html', {'form': add_quote_form})
 
 
 def top_quotes(request):
